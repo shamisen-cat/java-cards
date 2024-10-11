784e28078b443a694335b687f0d38073bd90f666
shamisen-cat <176907131+shamisen-cat@users.noreply.github.com>
2024-10-12 01:54:02 +0900

    refactor

--- src/test/ScoreTest.java
+++ src/test/ScoreTest.java
@@ -5,25 +5,45 @@ import score.Score;
 
 public class ScoreTest {
     public static void main(String[] args) {
+        GameResultEnum WIN = GameResultEnum.WIN;
+        GameResultEnum LOSE = GameResultEnum.LOSE;
+        GameResultEnum DRAW = GameResultEnum.DRAW;
+        
         System.out.println("Checking the enum settings.");
-        System.out.println("WIN: " + GameResultEnum.WIN.getMessage());
-        System.out.println("LOSE: " + GameResultEnum.LOSE.getMessage());
-        System.out.println("DRAW: " + GameResultEnum.DRAW.getMessage());
+        System.out.println(WIN + ": " + WIN.getMessage());
+        System.out.println(LOSE + ": " + LOSE.getMessage());
+        System.out.println(DRAW + ": " + DRAW.getMessage());
         
         System.out.println("Generating a new score.");
         Score score = new Score();
-        System.out.println("WIN: " + score.getWinCount() + " LOSE: " + score.getLoseCount() + " DRAW: " + score.getDrawCount());
+        System.out.println(
+                     WIN + ": " + score.getWin()
+            + " " + LOSE + ": " + score.getLose()
+            + " " + DRAW + ": " + score.getDraw()
+        );
         
         System.out.println("If the game result is a win.");
-        score.setGameResult(GameResultEnum.WIN);
-        System.out.println("WIN: " + score.getWinCount() + " LOSE: " + score.getLoseCount() + " DRAW: " + score.getDrawCount());
+        score.incrementScore(WIN);
+        System.out.println(
+                     WIN + ": " + score.getWin()
+            + " " + LOSE + ": " + score.getLose()
+            + " " + DRAW + ": " + score.getDraw()
+        );
         
         System.out.println("If the game result is a lose.");
-        score.setGameResult(GameResultEnum.LOSE);
-        System.out.println("WIN: " + score.getWinCount() + " LOSE: " + score.getLoseCount() + " DRAW: " + score.getDrawCount());
+        score.incrementScore(LOSE);
+        System.out.println(
+                     WIN + ": " + score.getWin()
+            + " " + LOSE + ": " + score.getLose()
+            + " " + DRAW + ": " + score.getDraw()
+        );
         
         System.out.println("If the game result is a draw.");
-        score.setGameResult(GameResultEnum.DRAW);
-        System.out.println("WIN: " + score.getWinCount() + " LOSE: " + score.getLoseCount() + " DRAW: " + score.getDrawCount());
+        score.incrementScore(DRAW);
+        System.out.println(
+                     WIN + ": " + score.getWin()
+            + " " + LOSE + ": " + score.getLose()
+            + " " + DRAW + ": " + score.getDraw()
+        );
     }
 }
